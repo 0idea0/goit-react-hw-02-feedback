33788884ae4a74febb89927dc672479e1fab0305
KYRYLO VOITOVSKYI <kirill.voitovskiy@gmail.com>
2021-12-08 11:52:44 +0200

    update App.js

--- src/App.js
+++ src/App.js
@@ -4,55 +4,43 @@ import FeedbackOptions from './components/FeedbackOptions';
 import Statistics from './components/Statistics';
 
 class App extends Component {
-  // Стейт
   state = {
     good: 0,
     neutral: 0,
     bad: 0,
   };
 
-  // Метод, который берёт названия кнопок и по ним прибавляет 1 к стейту
-  onLeaveFeedback = event => {
-    const label = event.target.textContent;
-
-    this.setState(prevState => ({ [label]: (prevState[label] += 1) }));
-  };
-
-  // Метод, который суммирует значения отзывов в стейте
   countTotalFeedback = () => {
-    const total = Object.keys(this.state).reduce(
-      (acc, value) => acc + this.state[value],
-      0,
-    );
-
-    return total;
+    const { good, bad, neutral } = this.state;
+    return good + neutral + bad;
   };
 
-  // Считает процент хороших отзывов (хорошие умножаем на 100 и делим на общее количество)
   countPositiveFeedbackPercentage = () => {
-    const percent = Math.round(
-      (this.state.good * 100) / this.countTotalFeedback(),
-    );
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    if (total) {
+      return `${Math.round((good / total) * 100)}`;
+    }
+    return '0%';
+  };
 
-    return percent;
+  incrementHandler = e => {
+    const key = e.target.name;
+    this.setState(prev => ({ [key]: prev[key] + 1 }));
   };
 
   render() {
-    const { good, neutral, bad } = this.state;
-
     return (
       <>
         <Section title="Please leave feedback">
           <FeedbackOptions
-            options={this.state} // Передаем в компонент кнопок весь стейт
-            onLeaveFeedback={this.onLeaveFeedback} // Метод для названий кнопок и инкримента
+            options={['good', 'neutral', 'bad']} // Передаем в компонент кнопок весь стейт
+            onIncrement={this.incrementHandler} // Метод для названий кнопок и инкримента
           />
         </Section>
         <Section title="Statistics">
           <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
+            {...this.state}
             total={this.countTotalFeedback} // Передаем в компонент статистики подсчет всех отзывов
             positivePercentage={this.countPositiveFeedbackPercentage} // Туда же передаем подсчет хороших отзывов
           />
